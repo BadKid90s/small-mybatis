442ae0524f7f7e57df305cfc927b7055dfb4fd11
王瑞玉 <wangruiyu@bjknrt.com>
2022-08-16 18:20:51 +0800

    feature: 实现映射器的注册和使用

--- src/test/java/com/wry/mybatis/test/ApiTest.java
+++ src/test/java/com/wry/mybatis/test/ApiTest.java
@@ -6,6 +6,7 @@ import com.wry.mybatis.session.SqlSession;
 import com.wry.mybatis.session.SqlSessionFactory;
 import com.wry.mybatis.session.impl.DefaultSqlSessionFactory;
 import com.wry.mybatis.test.dao.IUserDao;
+import org.apache.ibatis.binding.MapperProxy;
 import org.junit.jupiter.api.Test;
 
 
@@ -13,6 +14,9 @@ public class ApiTest {
 
     @Test
     public void test_MapperProxyFactory() {
+        //新版本jdk使用这个
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
+
         // 1. 注册 Mapper
         MapperRegistry registry = new MapperRegistry();
         registry.addMappers("com.wry.mybatis.test.dao");
@@ -27,7 +31,5 @@ public class ApiTest {
         // 4. 测试验证
         String res = userDao.queryUserName("10001");
         StaticLog.info("测试结果：{}", res);
-
     }
-
 }
